93d575a567f8414c732987a07f5a9562873cd1a7
Justin <justinreusnow@gmail.com>
2017-03-04 12:40:04 -0600

    Cyto is working

--- src/roadmap/roadmap.js
+++ src/roadmap/roadmap.js
@@ -1,37 +1,75 @@
-var cy = cytoscape({
-    // very commonly used options
-    container: undefined,
-    elements: [ /* ... */ ],
-    style: [ /* ... */ ],
-    layout: { name: 'grid' /* , ... */ },
+$(function(){
 
-    // initial viewport state:
-    zoom: 1,
-    pan: { x: 0, y: 0 },
+    var cy = window.cy = cytoscape({
+        container: document.getElementById('cy'),
 
-    // interaction options:
-    minZoom: 1e-50,
-    maxZoom: 1e50,
-    zoomingEnabled: true,
-    userZoomingEnabled: true,
-    panningEnabled: true,
-    userPanningEnabled: true,
-    boxSelectionEnabled: false,
-    selectionType: 'single',
-    touchTapThreshold: 8,
-    desktopTapThreshold: 4,
-    autolock: false,
-    autoungrabify: false,
-    autounselectify: false,
+        boxSelectionEnabled: false,
+        autounselectify: true,
 
-    // rendering options:
-    headless: false,
-    styleEnabled: true,
-    hideEdgesOnViewport: false,
-    hideLabelsOnViewport: false,
-    textureOnViewport: false,
-    motionBlur: false,
-    motionBlurOpacity: 0.2,
-    wheelSensitivity: 1,
-    pixelRatio: 'auto'
+        layout: {
+            name: 'dagre'
+        },
+
+        style: [
+            {
+                selector: 'node',
+                style: {
+                    'content': 'data(id)',
+                    'text-opacity': 0.5,
+                    'text-valign': 'center',
+                    'text-halign': 'right',
+                    'background-color': '#11479e'
+                }
+            },
+
+            {
+                selector: 'edge',
+                style: {
+                    'width': 4,
+                    'target-arrow-shape': 'triangle',
+                    'line-color': '#9dbaea',
+                    'target-arrow-color': '#9dbaea',
+                    'curve-style': 'bezier'
+                }
+            }
+        ],
+
+        elements: {
+            nodes: [
+                { data: { id: 'n0' } },
+                { data: { id: 'n1' } },
+                { data: { id: 'n2' } },
+                { data: { id: 'n3' } },
+                { data: { id: 'n4' } },
+                { data: { id: 'n5' } },
+                { data: { id: 'n6' } },
+                { data: { id: 'n7' } },
+                { data: { id: 'n8' } },
+                { data: { id: 'n9' } },
+                { data: { id: 'n10' } },
+                { data: { id: 'n11' } },
+                { data: { id: 'n12' } },
+                { data: { id: 'n13' } },
+                { data: { id: 'n14' } },
+                { data: { id: 'n15' } },
+                { data: { id: 'n16' } }
+            ],
+            edges: [
+                { data: { source: 'n0', target: 'n1' } },
+                { data: { source: 'n1', target: 'n2' } },
+                { data: { source: 'n1', target: 'n3' } },
+                { data: { source: 'n4', target: 'n5' } },
+                { data: { source: 'n4', target: 'n6' } },
+                { data: { source: 'n6', target: 'n7' } },
+                { data: { source: 'n6', target: 'n8' } },
+                { data: { source: 'n8', target: 'n9' } },
+                { data: { source: 'n8', target: 'n10' } },
+                { data: { source: 'n11', target: 'n12' } },
+                { data: { source: 'n12', target: 'n13' } },
+                { data: { source: 'n13', target: 'n14' } },
+                { data: { source: 'n13', target: 'n15' } },
+            ]
+        },
+    });
+
 });
