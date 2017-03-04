f9fafb7e9afc3c26af0427d10f6e5801b01b6bc0
Justin <justinreusnow@gmail.com>
2017-03-04 12:31:20 -0600

    Basic graph added

--- src/roadmap/roadmap.js
+++ src/roadmap/roadmap.js
@@ -1,3 +1,37 @@
-/**
- * Created by Justin on 03/04/2017.
- */
+var cy = cytoscape({
+    // very commonly used options
+    container: undefined,
+    elements: [ /* ... */ ],
+    style: [ /* ... */ ],
+    layout: { name: 'grid' /* , ... */ },
+
+    // initial viewport state:
+    zoom: 1,
+    pan: { x: 0, y: 0 },
+
+    // interaction options:
+    minZoom: 1e-50,
+    maxZoom: 1e50,
+    zoomingEnabled: true,
+    userZoomingEnabled: true,
+    panningEnabled: true,
+    userPanningEnabled: true,
+    boxSelectionEnabled: false,
+    selectionType: 'single',
+    touchTapThreshold: 8,
+    desktopTapThreshold: 4,
+    autolock: false,
+    autoungrabify: false,
+    autounselectify: false,
+
+    // rendering options:
+    headless: false,
+    styleEnabled: true,
+    hideEdgesOnViewport: false,
+    hideLabelsOnViewport: false,
+    textureOnViewport: false,
+    motionBlur: false,
+    motionBlurOpacity: 0.2,
+    wheelSensitivity: 1,
+    pixelRatio: 'auto'
+});
