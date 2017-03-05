dc9e94b347e9f0330c886646f4472df9a71b4dba
Chau Tran <ctch5@mail.umsl.edu>
2017-03-04 18:17:26 -0600

    donut chart added home.html

--- src/vendor/morrisjs/morris.js
+++ src/vendor/morrisjs/morris.js
@@ -1890,3 +1890,12 @@ Licensed under the BSD-2-Clause License.
   })(Morris.EventEmitter);
 
 }).call(this);
+
+Morris.Donut({
+  element: 'morris-donut-chart',
+  data: [
+    {label: "Download Sales", value: 12},
+    {label: "In-Store Sales", value: 30},
+    {label: "Mail-Order Sales", value: 20}
+  ]
+});
